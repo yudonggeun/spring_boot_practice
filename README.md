3d7f6c70b50d4a640c60e2504483a8e55902874e
yudonggeun <ydong98@gmail.com>
2023-10-15 21:27:13 +0900

    1장 실습

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-# Getting Started
+# ABOUT
 
-### Reference Documentation
-For further reference, please consider the following sections:
-
-* [Official Gradle documentation](https://docs.gradle.org)
-* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.1.4/gradle-plugin/reference/html/)
-* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.1.4/gradle-plugin/reference/html/#build-image)
-
-### Additional Links
-These additional references should also help you:
-
-* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
-
+`실전 스프링 부트` 책의 내용을 실습하고 익히는 저장소입니다.
--- src/main/java/com/example/spring_boot_example/SpringBootExampleApplication.java
+++ src/main/java/com/example/spring_boot_example/SpringBootExampleApplication.java
@@ -2,12 +2,28 @@ package com.example.spring_boot_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+//
+        var springApp = new SpringApplication(SpringBootExampleApplication.class);
+        springApp.addListeners((ApplicationListener<ApplicationStartingEvent>) event -> {
+            System.out.println("Application Starting Event logged at " + new Date(event.getTimestamp()));
+        });
+        springApp.run(args);
+    }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationReadyEvent(ApplicationReadyEvent applicationReadyEvent) {
+        System.out.println("Application Ready Event generated at " + new Date(applicationReadyEvent.getTimestamp()));
+    }
 }
